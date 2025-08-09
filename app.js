45670ad48d34c757c88e5c1519599b2edfaede34
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-09 23:58:46 +0530

    user controller created

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ const cors =require("cors")
 const app = express();
 
 const connectDB = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+
+
+
+
+
+
 connectDB();
 
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.get("/", (req, res) => {
   res.send(`hello world`);
 });
+app.use("/users",userRoutes)
+
+
 
 module.exports = app; 
  
