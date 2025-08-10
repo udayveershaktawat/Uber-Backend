d1178aebd15e78807aac527d1704654e39a93d07
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-10 23:42:35 +0530

    blacklistToken schema created succesfully

---
+++ models/blacklistToken.model.js
@@ -0,0 +1,18 @@
+const mongoose = requrie("mongoose");
+
+
+const blacklistTokenSchema = new mongoose.Schema({
+    token:{
+        type:String,
+        required:true,
+        unique:true,
+
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now();,
+        expires:86400
+    }
+});
+
+module.exports = mongoose.model("blacklistToken",blacklistTokenSchema)
